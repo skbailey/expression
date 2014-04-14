59ace504d542301f8a1f00e035a4ed763ae1aa44
Sherard Bailey <sherard@gmail.com>
2014-04-13 21:44:50 -0400

    updated interface

--- public/javascripts/lib/config.js
+++ public/javascripts/lib/config.js
@@ -3,6 +3,7 @@ requirejs.config({
 		jquery: "http://code.jquery.com/jquery-1.11.0.min",
     underscore: "http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min",
     backbone: "http://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min",
+    bootstrap: "http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js",
     formparams: "../vendor/jquery.formparams",
     form: "views/form",
     "expression-model": "models/expression",
@@ -29,7 +30,10 @@ requirejs.config({
   }
 });
 
-require(["jquery", "form"], function($, FormView){
+require([
+  "jquery", 
+  "bootstrap",
+  "form"], function($, FormView){
   $(function($){
     console.log("the page has loaded");
     window.myform = new FormView({el: "#content"});
